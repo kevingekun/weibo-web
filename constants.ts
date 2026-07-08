d6f3fe9e809eca680a46f3b8ab9606b0aefdbfe3
18005036 <gekk@haier.com>
2026-07-08 11:04:14 +0800

    修改后端地址

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://127.0.0.1:8060/api/v1';
+//export const API_BASE_URL = 'http://127.0.0.1:8060/api/v1';
+export const API_BASE_URL = 'https://weibo.102181.xyz/api/v1';
